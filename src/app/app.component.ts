a3487975f323c1c0ef33e8824fa3a1267df4993a
Ujjaval Verma <ujjavalverma10@gmail.com>
2022-08-05 19:46:42 +0530

    add express backend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'govideo-twilio';
+  messages$;
+  constructor(private http: HttpClient) {
+  }
+
+  post() {
+    this.http.post<any>('http://localhost:4201/users', {name: 'Ujjaval', cred:'Developer'})
+    .subscribe(next => console.log(next));
+  }
+
+  get() {
+    this.messages$ = this.http.get<any[]>('http://localhost:4201', {observe: 'response', responseType: 'json'});
+    /*this.http.get<any[]>('http://localhost:4201').subscribe(res => {
+      console.log(res);
+      this.messages$ = res;
+    });*/
+  }
 }
